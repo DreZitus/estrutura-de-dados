b3baf8e3c74daa4f0c990afe5a9f377bbe582016
André <108902451+DreZitus@users.noreply.github.com>
2025-11-08 02:08:14 +0000

    adicionando um método para inserir  jogos no início da pilha e um método para mostrar os jogos.

--- LIFO.php
+++ LIFO.php
@@ -10,4 +10,26 @@ class Pilha
         $this->pilha = [];
         $this->tamaho = 0;
     }
+
+
+    public function adicionarNoinicio(string $nomeJogo): void
+    {
+        array_unshift($this->pilha, $nomeJogo);
+        #this->tamanho++;
+    }
+
+    public function mostrarItens(): void
+    {   foreach ($this->pilha as $item) {
+            print_r("nome do jogo => {$item}. \n");
+        }
+    
+    }
 }
+
+$pilhaDEJogos = new pilha();
+
+$pilhaDEJogos->adicionarNoInicio ("god of war");
+$pilhaDEJogos->adicionarNoInicio ("PES");
+$pilhaDEJogos->adicionarNoInicio ("rule of rose");
+$pilhaDEJogos->adicionarNoInicio ("GTA 5");
+$pilhaDEJogos->mostrarItens();
