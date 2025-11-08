8148c73db0b17ccfedc73ee1bc7d47373e7977a7
André <108902451+DreZitus@users.noreply.github.com>
2025-11-08 01:59:46 +0000

    adicionando atributos de array  contendo itens da pilha e o outro do tipo inteiro contendo o tamanho da pilha

---
+++ LIFO.php
@@ -0,0 +1,13 @@
+<?php
+
+class Pilha
+{
+    private array $pilha;
+    private int $tamanho;
+
+    public function __contruct()
+    {
+        $this->pilha = [];
+        $this->tamaho = 0;
+    }
+}
